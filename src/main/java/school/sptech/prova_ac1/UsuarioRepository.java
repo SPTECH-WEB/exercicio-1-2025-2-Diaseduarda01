9783133c628d526a9e4d8cd86bc14f14ffb32970
Eduarda Dias <maria.dias@sptech.school>
2025-09-16 09:47:30 -0300

    feito

--- src/main/java/school/sptech/prova_ac1/UsuarioRepository.java
+++ src/main/java/school/sptech/prova_ac1/UsuarioRepository.java
@@ -1,5 +1,13 @@
 package school.sptech.prova_ac1;
 
-public interface UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    List<Usuario> findByEmailOrCpf(String email, String cpf);
+    List<Usuario> findByDataNascimentoGreaterThan(LocalDate dataNascimento);
 }
